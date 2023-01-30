48cf7cb634962ab78b6e5af27c9f10005b0b82b6
Raj Mhatre <rajjimh18phpcomp@student.mes.ac.in>
2023-01-30 23:59:55 +0530

    Recent Work

--- js/index.js
+++ js/index.js
@@ -1,11 +1,56 @@
 // Navbar
 $(function () {
-    var navbar = $(".header-inner");
-    $(window).scroll(function () {
-      if ($(window).scrollTop() <= 40) {
-        navbar.removeClass("navbar-scroll");
-      } else {
-        navbar.addClass("navbar-scroll");
-      }
-    });
+  var navbar = $(".header-inner");
+  $(window).scroll(function () {
+    if ($(window).scrollTop() <= 40) {
+      navbar.removeClass("navbar-scroll");
+    } else {
+      navbar.addClass("navbar-scroll");
+    }
   });
+});
+
+// Slider
+
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+// Auto Slide   if you need auto slide ,remove the commit "//"
+//var slideIndex = 0;
+//showSlides();
+//function showSlides() {
+//var i;
+//  var slides = document.getElementsByClassName("mySlides");
+// for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+// }
+//  slideIndex++;
+// if (slideIndex > slides.length) { slideIndex = 1 }
+// slides[slideIndex - 1].style.display = "block";
+// setTimeout(showSlides, 2000); // Change image every 2 seconds
+//}
+
